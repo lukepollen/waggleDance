7790e752c5d6ddc7957468188be944edf61752b6
Luke Pollen <lukepollen@searchpollen.co.uk>
2024-03-09 01:38:21 +0700

    Update talkToGemma.py
    Edited to remove pizza references.

--- talkToGemma.py
+++ talkToGemma.py
@@ -4,17 +4,17 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 ### End of Imports ###
 
-# Mmm, pizza. Like it, we do. Test Large Language Model, we shall. Pineapple on pizza, glorious, it is.
+# Test Google's Gemma-2b, the Large Language Model. 
 if __name__ == 'main':
 
-    # Download from https://huggingface.co/google/gemma-2b/tree/main . Afterm change this to your download location.
+    # Download from https://huggingface.co/google/gemma-2b/tree/main . After, change this to your download location.
     twoBConfigPath = 'C:/Users/lukep/OneDrive/workandplay/Logic and Software/Computer Science/LLM/gemma/2b'
 
     # Load the main components to respond to our text, a tokenizer to convert text to numeric, and a model to reverse.
     tokenizer = AutoTokenizer.from_pretrained(twoBConfigPath, local_files_only=True)
     model = AutoModelForCausalLM.from_pretrained(twoBConfigPath, local_files_only=True)
 
-    # Test query to test Gemma with. MMMM, delicious olives and pineapple toppings, force strong in it is.
+    # Test query to test Gemma with. 
     input_text = "What is the derivative of X to the power of 2?"
 
     # Convert the text to a tensor of integers and then send to the model to get a response.
