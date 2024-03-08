20821bff44a837dbd0182cbccd37fdfd322ba212
Luke Pollen <lukepollen@searchpollen.co.uk>
2024-03-09 01:36:30 +0700

    Add files via upload
    Uploaded a Python script which, when given a correctly directory where gemma-2b.gguf lives and the config.json files are, will load Gemma using the transformers library.

---
+++ talkToGemma.py
@@ -0,0 +1,23 @@
+### Imports ###
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+### End of Imports ###
+
+# Mmm, pizza. Like it, we do. Test Large Language Model, we shall. Pineapple on pizza, glorious, it is.
+if __name__ == 'main':
+
+    # Download from https://huggingface.co/google/gemma-2b/tree/main . Afterm change this to your download location.
+    twoBConfigPath = 'C:/Users/lukep/OneDrive/workandplay/Logic and Software/Computer Science/LLM/gemma/2b'
+
+    # Load the main components to respond to our text, a tokenizer to convert text to numeric, and a model to reverse.
+    tokenizer = AutoTokenizer.from_pretrained(twoBConfigPath, local_files_only=True)
+    model = AutoModelForCausalLM.from_pretrained(twoBConfigPath, local_files_only=True)
+
+    # Test query to test Gemma with. MMMM, delicious olives and pineapple toppings, force strong in it is.
+    input_text = "What is the derivative of X to the power of 2?"
+
+    # Convert the text to a tensor of integers and then send to the model to get a response.
+    input_ids = tokenizer(input_text, return_tensors='pt')
+    outputs = model.generate(**input_ids, max_length=128) # Tweak max_length for longer response.
+    print(tokenizer.decode(outputs[0])) # Print the response.
